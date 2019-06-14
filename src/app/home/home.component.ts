2c5c340ea0585343a717b948f359c06ad219303b
Zach Weintraub <zachweintraub@gmail.com>
2019-06-14 12:15:08 -0700

    add child components to homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  introBgImg: string = '../../assets/img/virgin-suicides.jpg';
+  
   constructor() { }
-
+  
   ngOnInit() {
   }
-
+  
 }
