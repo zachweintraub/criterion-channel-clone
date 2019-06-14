36b776f4ca0431bcc7429058c25e33da0a9837be
Zach Weintraub <zachweintraub@gmail.com>
2019-06-14 15:29:21 -0700

    style salespitch component and implement logic to select different displays

--- src/app/home/salespitch/salespitch.component.ts
+++ src/app/home/salespitch/salespitch.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SalespitchComponent implements OnInit {
 
+  selectedDisplay: string = 'left';
+
   constructor() { }
 
   ngOnInit() {
