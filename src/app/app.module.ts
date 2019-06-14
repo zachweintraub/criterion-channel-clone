c1474168e86e0b54b1d53f770fd6c9950ba5c9d9
Zach Weintraub <zachweintraub@gmail.com>
2019-06-14 11:52:55 -0700

    populate home header html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NowPlayingComponent } from './now-playing/now-playing.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './home/header/header.component';
 import { IntroComponent } from './home/intro/intro.component';
 import { SalespitchComponent } from './home/salespitch/salespitch.component';
 import { FooterComponent } from './home/footer/footer.component';
