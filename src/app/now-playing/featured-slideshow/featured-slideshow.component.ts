5f86c02d92e26fcc30b2414af263e3acdb1fc569
Zach Weintraub <zachweintraub@gmail.com>
2019-06-21 11:53:19 -0700

    still working on the carousels :(

--- src/app/now-playing/featured-slideshow/featured-slideshow.component.ts
+++ src/app/now-playing/featured-slideshow/featured-slideshow.component.ts
@@ -25,7 +25,7 @@ export class FeaturedSlideshowComponent implements OnInit {
   ngOnInit() {
     this.featuredCollections = this.collectionService.getCollections();
     this.currentSlide = 'slide0';
-    setTimeout(()=>{this.selectSlide('0')},400);
+    setTimeout(()=>{this.selectSlide('0')},1000);
   }
   
 }
